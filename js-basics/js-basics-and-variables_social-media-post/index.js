3b510d84e748fe2b0435749cf51ce4a017c24d93
Emanuel <emphaser91@gmail.com>
2025-02-12 22:45:15 +0100

    Merge-Konflikt in styles.css gelöst

---
+++ js-basics/js-basics-and-variables_social-media-post/index.js
@@ -0,0 +1,36 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a constant variable for each data point listed below:
+	- a title for the post
+	- text content for the post
+	- the number of likes the post has received
+	- the user who created the post
+	- a boolean called `isReported` to indicate whether the post has been reported
+*/
+
+// --v-- write your code here --v--
+const title = "Mein erster Tag als Web-Entwickler";
+console.log(title);
+
+// --^-- write your code here --^--
+const textContent =
+  "Ich habe heute begonnen, das HTML & CSS für meine Webseite zu entwickeln";
+console.log(textContent);
+let likes = 121;
+likes = likes + 1;
+console.log(likes);
+
+const user = "Emanuel";
+console.log(user);
+
+const isReported = true;
+console.log(isReported);
+
+/*
+2. Log all variables to the console. Then increase the number of likes by one and log the updated like count. Modify your code from step 1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(likes);
+// --^-- write your code here --^--
