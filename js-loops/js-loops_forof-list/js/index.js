4447e526e29989f8a97c00deca63fd91f8f3cea1
Emanuel <emphaser91@gmail.com>
2025-02-20 12:22:18 +0100

    erster commit

---
+++ js-loops/js-loops_forof-list/js/index.js
@@ -0,0 +1,25 @@
+console.clear();
+
+const main = document.querySelector('[data-js="main"]');
+
+const ol = document.createElement("ol");
+main.append(ol);
+
+const programmingLanguages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "C#",
+  "C++",
+  "PHP",
+  "Ruby",
+];
+
+// --v-- write or modify code below this line --v--
+
+for (const ItemOfArray of programmingLanguages) { // Alle ELemente aus dem Array werden von oben nach unten durchlaufen 
+  
+
+}
+
+// --^-- write or modify code above this line --^--
