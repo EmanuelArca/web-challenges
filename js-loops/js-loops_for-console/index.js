e21684fc449a936f454cf633e1fe4fc908f3b50f
Emanuel <emphaser91@gmail.com>
2025-02-20 12:24:14 +0100

    fertig

--- js-loops/js-loops_for-console/index.js
+++ js-loops/js-loops_for-console/index.js
@@ -1,6 +1,6 @@
 console.clear();
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 0; i <= 100; i++) { // man kann auch statt i etwas anderes wählen, z.B Counter/Number etc. egal was 
 console.log(i);
 
 }
