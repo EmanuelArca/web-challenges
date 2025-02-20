5d5108a0a3e8a67ec48033244ee0a42f389efe01
Emanuel <emphaser91@gmail.com>
2025-02-20 14:28:34 +0100

    neue änderung

--- js-loops/js-loops_forof-list/js/index.js
+++ js-loops/js-loops_forof-list/js/index.js
@@ -16,10 +16,13 @@ const programmingLanguages = [
 ];
 
 // --v-- write or modify code below this line --v--
-
-for (const ItemOfArray of programmingLanguages) { // Alle ELemente aus dem Array werden von oben nach unten durchlaufen 
   
-
+  for (const ItemOfArray of programmingLanguages) {    // Alle Elemente aus dem Array werden von oben nach unten durchlaufen, Loop wird erstellt
+    const li = document.createElement("li")    // Neue Variable li erstellen und speichern um sie weiter zu verwenden, durch createElement wird "li" hinzugefügt
+    ol.append(li);    // dem ol element wird ein neues li hinzugefügt für jeden Durchlauf der Schleife
+    li.textContent = "Programmiersprache:" + ItemOfArray;    // Für jedes hinzugefügte li wird der Name mit ausgegeben der einzelnen Items im Array
+    
+    // alternativ geht auch  li.textContent = `Programmiersprache: ${[ItemofArray]}`;
 }
 
 // --^-- write or modify code above this line --^--
