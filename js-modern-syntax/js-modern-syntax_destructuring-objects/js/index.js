65b82d57f4722df3434bb4704a422de9f258b1fc
Emanuel <emphaser91@gmail.com>
2025-02-25 12:09:08 +0100

    aufgabe hinzugefügt

---
+++ js-modern-syntax/js-modern-syntax_destructuring-objects/js/index.js
@@ -0,0 +1,87 @@
+console.clear();
+
+/*
+EXERCISE 1
+Extract only the course name from the `company` object.
+Make sure to export your variable as shown in the example below for the tests to work.
+*/
+
+const company = {
+  name: "TechAcademy",
+  course: "Web Development",
+  location: "Hamburg",
+};
+
+const { course } = company;      // destructured, um auf course zuzugreifen
+
+console.log(course);            // hiermit testen ob es klappt, er loggt "web development" aus
+
+// Example: export const { value1 } = myObject;
+
+/*
+EXERCISE 2
+We have an object called `user`.
+Use destructuring to extract the following:
+ 2.1: The `name` property into a variable `name`.
+ 2.2: The `years` property into a variable `age`.
+ 2.3: The `isAdmin` property into the variable `isAdmin`.
+      Set a default value of `false` if `isAdmin` is not present.
+*/
+
+const user = { 
+  name: "John", 
+  years: 30 
+};
+
+const { name } = user;            // name bleibt gleich
+const { years: age } = user;       // durch :age wird es umbenannt
+const { isAdmin = false } = user;  // durch =false wird die neue isAdmin hinzugefügt, sie existiert vorher nicht  
+
+console.log(name);                 // als test. bei age drauf achten dass man nicht years trotzdem eingibt, es wurde ja geändert
+console.log(age);
+console.log(isAdmin);
+
+
+/*
+EXERCISE 3
+Extract properties from the dog object and
+rename them as `dogName`, `dogBreed`, and `dogAge`, respectively.
+*/
+
+const dog = {
+  name: "Pluto",
+  breed: "husky",
+  age: 5,
+};
+
+/*
+EXERCISE 4
+Extract the `lastName` property from the `person` object as `personLastName`.
+Store all other properties in an object called `moreInformation`.
+*/
+
+const person = {
+  lastName: "Main",
+  age: 24,
+  firstName: "Alex",
+};
+
+/*
+EXERCISE 5
+Refactor the following function to use destructuring assignment for the
+three variables `name`, `country` and `numPeople`.
+Hint: You may need to rename one property during destructuring.
+*/
+
+export function logInfo(city) {
+  const name = city.name;
+  const country = city.country;
+  const numPeople = city.population;
+
+  return `${name} is in ${country} and has ${numPeople} inhabitants in it.`;
+}
+
+// Usage example:
+console.log(
+  logInfo({ name: "Marseille", country: "France", population: 861635 })
+);
